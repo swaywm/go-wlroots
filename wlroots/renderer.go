51864debae27415e36f33068c2ca6d37b054087b
John Chadwick <john@jchw.io>
2025-05-14 22:46:43 -0400

    Drop obsolete wlr_renderer_* calls

--- wlroots/renderer.go
+++ wlroots/renderer.go
@@ -29,28 +29,3 @@ func (r Renderer) OnDestroy(cb func(Renderer)) {
 func (r Renderer) InitDisplay(display Display) {
 	C.wlr_renderer_init_wl_display(r.p, display.p)
 }
-
-func (r Renderer) Begin(output Output, width int, height int) {
-	C.wlr_renderer_begin(r.p, C.uint(width), C.uint(height))
-}
-
-func (r Renderer) Clear(color *Color) {
-	c := color.toC()
-	C.wlr_renderer_clear(r.p, &c[0])
-}
-
-func (r Renderer) End() {
-	C.wlr_renderer_end(r.p)
-}
-
-func (r Renderer) RenderTextureWithMatrix(texture Texture, matrix *Matrix, alpha float32) {
-	m := matrix.toC()
-	C.wlr_render_texture_with_matrix(r.p, texture.p, &m[0], C.float(alpha))
-}
-
-func (r Renderer) RenderRect(box *GeoBox, color *Color, projection *Matrix) {
-	b := box.toC()
-	c := color.toC()
-	pm := projection.toC()
-	C.wlr_render_rect(r.p, &b, &c[0], &pm[0])
-}
