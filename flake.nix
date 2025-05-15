efe4f8c27b8c40e8cdcc79bcfd5856179cf3a0be
John Chadwick <john@jchw.io>
2025-05-14 23:04:36 -0400

    Modernize + update Nix flake
    Use `buildGoModule`
    Avoid using `with` and `rec` as they are no longer best practice
    Remove usage of gomod2nix as it is no longer needed
    Update all dependencies
    Apply formatting with nixfmt

--- flake.nix
+++ flake.nix
@@ -2,70 +2,65 @@
   description = "Nix flake for go-wlroots";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
-    gomod2nix = {
-      url = "github:tweag/gomod2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, gomod2nix }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ gomod2nix.overlays.default ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        tinywl = pkgs.buildGoModule {
+          pname = "tinywl";
+          version = "0.0.0";
+          src = self;
+          vendorHash = "sha256-0vsi8q/+H7I5qvyr9sIZFLD30yZivySLWYT6KrJqIcc=";
+          subPackages = [ "cmd/tinywl" ];
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.wayland-scanner
+          ];
+          buildInputs = [
+            pkgs.libxkbcommon
+            pkgs.pixman
+            pkgs.wlroots
+            pkgs.wayland
+            pkgs.libGL
+            pkgs.udev
+            pkgs.xorg.libX11
+            pkgs.xorg.xcbutilwm
+          ];
+          preBuild = ''
+            wayland-scanner private-code ${pkgs.wayland-protocols}/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml wlroots/xdg-shell-protocol.c
+            wayland-scanner server-header ${pkgs.wayland-protocols}/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml wlroots/xdg-shell-protocol.h
+          '';
         };
-        libxkbcommon =
-          pkgs.libxkbcommon.overrideAttrs (finalAttrs: prevAttrs: rec {
-            version = "1.6.0";
-            src = pkgs.fetchurl {
-              url = "https://xkbcommon.org/download/${prevAttrs.pname}-${version}.tar.xz";
-              hash = "sha256-DtwU7M3TkVFEWLxfWkuZhj7S1lHk3XYakKv09G75nCs=";
-            };
-          });
       in
       {
-        packages = rec {
+        packages = {
+          inherit tinywl;
           default = tinywl;
-          tinywl = with pkgs; buildGoApplication {
-            pname = "tinywl";
-            version = "v0.0.0";
-            src = ./.;
-            subPackages = "cmd/tinywl";
-            modules = ./gomod2nix.toml;
-            nativeBuildInputs = [ pkg-config wayland ];
-            buildInputs = [
-              libxkbcommon
-              pixman
-              wlroots
-              wayland
-              libGL
-              udev
-              xorg.libX11
-              xorg.xcbutilwm
-            ];
-            preBuild = ''
-              wayland-scanner private-code ${wayland-protocols}/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml wlroots/xdg-shell-protocol.c
-              wayland-scanner server-header ${wayland-protocols}/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml wlroots/xdg-shell-protocol.h
-            '';
-          };
         };
-        devShells.default = with pkgs; mkShell {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ tinywl ];
           buildInputs = [
-            gcc
-            go
-            gopls
-            gomod2nix.packages.${system}.default
-            libxkbcommon
-            wlroots
-            pkg-config
-            wayland
-            udev
-            libGL
-            pixman
-            xorg.libX11
-            xorg.xcbutilwm
+            pkgs.gcc
+            pkgs.go
+            pkgs.gopls
+            pkgs.libxkbcommon
+            pkgs.wlroots
+            pkgs.pkg-config
+            pkgs.wayland
+            pkgs.udev
+            pkgs.libGL
+            pkgs.pixman
+            pkgs.xorg.libX11
+            pkgs.xorg.xcbutilwm
           ];
         };
       }
